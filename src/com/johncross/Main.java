7d982881591a0b7ce7dc547ad85c208e97e7baae
John Cross <jwcross1@dmacc.edu>
2019-05-31 10:06:38 -0500

    first commit

---
+++ src/com/johncross/Main.java
@@ -0,0 +1,26 @@
+package com.johncross;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // convert a given number of pounds to kilograms
+
+        double numOfPounds = 200d;
+        double poundsToKilos = numOfPounds * 0.45359237d;
+        System.out.println(numOfPounds + " pounds = " + poundsToKilos + " kilos.");
+
+
+//        // width of int = 32 (4 bytes)
+//        int myIntValue = 5 / 3;
+//        // width of float = 32 (4 bytes)
+//        float myFloatValue = 5f / 3f;
+//        // width of doubl = 64 (8 bytes)
+//        double myDoubleValue = 5d / 3d;
+//
+//        System.out.println("myIntValue = " + myIntValue);
+//        System.out.println("myFloatValue = " + myFloatValue);
+//        System.out.println("myDoubleValue = " + myDoubleValue);
+
+    }
+}
